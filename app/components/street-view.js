c9610d5b822cce114e24954a67a4dfbce166bb75
Ricardo Mercado <ricky@cometa.consulting>
2015-03-17 19:30:34 -0400

    fix bootstrap tabs bug

--- app/components/street-view.js
+++ app/components/street-view.js
@@ -4,8 +4,8 @@ export default Ember.Component.extend({
 	map: null,
 	lat: null,
 	lng: null,
-	height: "550px",
-	width: "400px",
+	height: "400px",
+	width: "600px",
 
 	didInsertElement: function(){
 		let lat = this.get('lat');
@@ -19,10 +19,14 @@ export default Ember.Component.extend({
 				heading: 165,
 				pitch: 0
 			},
-			zoom: 1
+			zoom: 0
 		};
 		this.$().css({width: width, height: height});
 		let streetView = new google.maps.StreetViewPanorama(this.$().get(0), options);
 		this.set('map', streetView);
+		//bootstrap tabs fix for grey display
+		$(document).on('shown.bs.tab', function () { 
+      google.maps.event.trigger(streetView, 'resize'); 
+    });
 	}
 });
