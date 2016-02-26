ae6497718576b39891e05edd6a4b94742d7ba297
Ilya Radchenko <ilya@burstcreations.com>
2016-02-26 15:18:06 -0500

    Fix typo

--- addon/components/street-view.js
+++ addon/components/street-view.js
@@ -105,7 +105,7 @@ export default Component.extend({
      "zoomControlOptions",
      "addressControl",
      "addressControlOptions",
-     "linksControl"
+     "linksControl",
      "fullscreenControl",
      "fullscreenControlOptions",
      "imageDateControl"
